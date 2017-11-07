352a6ae82ac4f4e09a508455f11e361e0011779d
mbrenesn <mbrenes@ictp.it>
2017-11-07 15:14:14 +0100

    Added initial state class

---
+++ src/InitialState/InitialState.h
@@ -0,0 +1,33 @@
+#ifndef __INITIAL_STATE_H
+#define __INITIAL_STATE_H
+
+#include "../Environment/Environment.h"
+#include "../Utils/Utils.h"
+#include "../Basis/Basis.h"
+
+class InitialState
+{
+  public:
+    // Methods
+    InitialState(const Environment &env,
+                 const Basis &basis);
+    ~InitialState();
+    InitialState(const InitialState &rhs);
+    InitialState &operator=(const InitialState &rhs);
+    // Members
+    Vec InitialVec;
+    void neel_initial_state(LLInt *int_basis);
+    void random_initial_state(LLInt *int_basis,
+                              bool wtime = false,
+                              bool verbose = false);
+
+  private:
+    unsigned int l_, n_;
+    PetscMPIInt mpirank_;
+    PetscMPIInt mpisize_;
+    LLInt basis_size_;
+    PetscInt nlocal_;
+    PetscInt start_;
+    PetscInt end_;
+};
+#endif
